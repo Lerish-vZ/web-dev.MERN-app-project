17d6a215bb862f5edac73a72a5ddd6180192f220
Lerish-vZ <lerishavz@gmail.com>
2024-01-16 13:46:43 +0200

    Update

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+app.use(...);
+
+const db = require("./app/models")
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database.");
+    })
 
 var corsOptions = {
   origin: "http://localhost:8081",
