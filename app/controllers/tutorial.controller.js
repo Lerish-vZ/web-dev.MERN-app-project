9dfabf88d78f4a4e2428e0765c7451ef2ae94e67
Lerish-vZ <lerishavz@gmail.com>
2024-01-17 15:31:26 +0200

    Update

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -3,7 +3,22 @@ const Tutorial = db.tutorials;
 
 //Create and Save a new Tutorial
 exports.create = (req, res) => {
+    //Validate request 
+    if (!req.body.title) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
 
+    //Create a Tutorial
+    const tutorial = new Tutorial({
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    });
+
+    //Save Tutorial in the database
+    tutorial
+        .save
 };
 
 //Find a single Tutorial with an id 
