7f817bfa5ea4024d2a057892f56c5a98e162ad9c
Lerish-vZ <lerishavz@gmail.com>
2024-01-29 12:06:41 +0200

    Update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require("./app/models");
 const app = express();
 
 var corsOptions = {
-  origin: "http://lvz-web-development-bezkoder-frontend.netlify.app/" //Check
+  origin: "https://lvz-web-development-bezkoder-frontend.netlify.app/" //Check
 };
 
 app.use(cors(corsOptions));
