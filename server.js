43d00af07cb1a6a2611679ec65f986fefa3c40f4
Lerish-vZ <lerishavz@gmail.com>
2024-01-29 12:44:16 +0200

    Update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require("./app/models");
 const app = express();
 
 var corsOptions = {
-  origin: "https://tutorial-project12.netlify.app/" //Check
+  origin: "https://lvz-web-dev-tutorial-app.netlify.app/" //Check
 };
 
 app.use(cors(corsOptions));
