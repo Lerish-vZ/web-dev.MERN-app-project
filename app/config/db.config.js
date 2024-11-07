a25c4a0fd9e386425f2b95485deac646b34239fe
Lerish-vZ <lerishavz@gmail.com>
2024-11-07 10:15:38 +0200

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://lerishavz:tK3KOLhiNFKvS4Qc@cluster0.vsl2cv2.mongodb.net/"
+    url: "mongodb+srv://lerishavz:lerishavz@cluster0.vsl2cv2.mongodb.net/"
   };
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require("./app/models");
 const app = express();
 
 var corsOptions = {
-  origin: "https://lvz-web-dev-tutorial-frontend.netlify.app"
+  origin: "https://lvz-web-dev-tutorial-frontend.netlify.app/"
 };
 
 app.use(cors(corsOptions));
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-require("./app/routes/turorial.routes.js")(app);
+require("./app/routes/tutorial.routes.js")(app);
 
 
 // set port, listen for requests
